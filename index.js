58dc96227ed40602b213cbd92e0f28c81a3b53ff
David Nanjila <nanjiladavid2@gmail.com>
2025-01-29 05:41:36 +0300

    Add the fauls section
    Signed-off-by: David Nanjila <nanjiladavid2@gmail.com>

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 let homeEl = document.getElementById('home-score');
 let awayEl = document.getElementById('away-score');
 let winnerEl = document.getElementById('winner');
+let faulHomeEl = document.getElementById("faul-home")
+let awayFaulEl= document.getElementById("faul-away")
 
+let awayFaul = 0
+let homeFaul = 0
 let homeScore = 0;
 let awayScore = 0;
 homeEl.textContent = homeScore;
 awayEl.textContent = awayScore;
+faulHomeEl.textContent = 0
+awayFaulEl.textContent = 0
 
 
 function winner(){
@@ -53,8 +59,20 @@ function addOneAway(){
 function reset(){
     homeScore = 0;
     awayScore = 0;
+    homeFaul = 0
+    awayFaul = 0
     winnerEl.textContent = "";
     homeEl.textContent = homeScore;
     awayEl.textContent = awayScore;
+    faulHomeEl.textContent = homeFaul
+    awayFaulEl.textContent = awayFaul
 }
+function addFaulHome(){
+    homeFaul += 1
+    faulHomeEl.textContent = homeFaul
+}
+function addFaulAway(){
+    awayFaul += 1
+    awayFaulEl.textContent = awayFaul 
 
+}
